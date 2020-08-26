7ef2218c569c6a61a149a081c26ba5bef20d0e38
Premal upadhyay <34483490+premaluu@users.noreply.github.com>
2020-08-26 23:34:35 +0530

    Second commit for authentication

--- app/src/main/java/com/example/smartstudyapp2/LoginActivity.java
+++ app/src/main/java/com/example/smartstudyapp2/LoginActivity.java
@@ -2,13 +2,16 @@ package com.example.smartstudyapp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        intent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
