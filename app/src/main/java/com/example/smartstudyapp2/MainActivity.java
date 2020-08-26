0025d93245b1310409c9bbb2877d842c1c243d14
Premal upadhyay <34483490+premaluu@users.noreply.github.com>
2020-08-26 23:00:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/smartstudyapp2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.smartstudyapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
